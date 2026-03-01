f1e95f2eb7d1d5c68523b9a121ac7b410a040e13
paulo <paulomantillaa@gmail.com>
2026-03-01 12:38:03 -0600

    Initial Commit

---
+++ profileInstagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  profileInstagram
+//
+//  Created by Paulo Mantilla on 01/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ profileInstagram/profileInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileInstagramApp.swift
+//  profileInstagram
+//
+//  Created by Paulo Mantilla on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct profileInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
